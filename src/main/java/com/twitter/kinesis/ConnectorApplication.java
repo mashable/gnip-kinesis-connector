2d6f1fddb1bb0d360a874e937fbb1d189453e96a
Roshan Bangera <roshan@mashable.com>
2015-10-08 20:32:12 -0400

    * add support for inserting replay streams

--- src/main/java/com/twitter/kinesis/ConnectorApplication.java
+++ src/main/java/com/twitter/kinesis/ConnectorApplication.java
@@ -12,6 +12,7 @@ import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.endpoint.EnterpriseStreamingEndpoint;
 import com.twitter.hbc.core.endpoint.RealTimeEnterpriseStreamingEndpoint;
+import com.twitter.hbc.core.endpoint.ReplayEnterpriseStreamingEndpoint;
 import com.twitter.hbc.core.processor.LineStringProcessor;
 import com.twitter.hbc.httpclient.auth.BasicAuth;
 import com.twitter.kinesis.metrics.HBCStatsTrackerMetric;
@@ -19,10 +20,15 @@ import com.twitter.kinesis.metrics.MetricReporter;
 import com.twitter.kinesis.metrics.ShardMetricLogging;
 import com.twitter.kinesis.metrics.SimpleMetricManager;
 import com.twitter.kinesis.utils.Environment;
+import org.apache.commons.cli.*;
+import org.joda.time.LocalDateTime;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Date;
 
 public class ConnectorApplication {
   private static final Logger logger = LoggerFactory.getLogger(ConnectorApplication.class);
@@ -30,19 +36,46 @@ public class ConnectorApplication {
   private Environment environment;
   private KinesisProducer producer;
   private SimpleMetricManager simpleMetricManager;
+  private Date replayFrom;
+  private Date replayTo;
 
   public ConnectorApplication() {
     environment = new Environment();
   }
 
   public static void main(String[] args) {
-    logger.info("Starting Connector Application...");
+    Options options = createOptions();
     try {
+      CommandLine cmdLine = new DefaultParser().parse(options, args);
+      if (cmdLine.hasOption("h")) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp( "java target/connector-1.0-jar-with-dependencies.jar", options);
+        System.exit(0);
+      }
+
+      logger.info("Starting Connector Application...");
       ConnectorApplication application = new ConnectorApplication();
+      if (cmdLine.hasOption("f") && cmdLine.hasOption("t")) {
+        application.setReplayDates(cmdLine.getOptionValue("f"), cmdLine.getOptionValue("t"));
+      }
+
       application.start();
     } catch (Exception e) {
       logger.error("Unexpected error occured", e);
     }
+  }
+
+  private static Options createOptions() {
+    Options options = new Options();
+    options.addOption(Option.builder("h").argName("help").hasArg(false).desc("Help").build());
+    options.addOption(Option.builder("f").argName("from").hasArg().desc("Replay from (iso8601 date)").type(Date.class).build());
+    options.addOption(Option.builder("t").argName("to").hasArg().desc("Replay to (iso8601 date)").type(Date.class).build());
+    return options;
+  }
+
+  private void setReplayDates(String from, String to) {
+    this.replayFrom = new LocalDateTime(new DateTime(from).toDate(), DateTimeZone.UTC).toDate();
+    this.replayTo = new LocalDateTime(new DateTime(to).toDate(), DateTimeZone.UTC).toDate();
   }
 
   private void configure() {
@@ -103,6 +136,10 @@ public class ConnectorApplication {
     String account = this.environment.accountName();
     String label = this.environment.streamLabel();
     String product = this.environment.product();
-    return new RealTimeEnterpriseStreamingEndpoint(account, product, label);
+    if (this.replayFrom != null && this.replayTo != null) {
+      return new ReplayEnterpriseStreamingEndpoint(account, product, label, this.replayFrom, this.replayTo);
+    } else {
+      return new RealTimeEnterpriseStreamingEndpoint(account, product, label);
+    }
   }
 }
